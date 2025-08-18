6410ad92e9a8d184a84b978169586d60b0145b4e
7DavidMedina7 <david.medina.alonzo@gmail.com>
2025-08-17 20:39:18 -0500

    Initial Commit

---
+++ YgoShuffle/YgoShuffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  YgoShuffleApp.swift
+//  YgoShuffle
+//
+//  Created by David Alonzo Medina on 8/17/25.
+//
+
+import SwiftUI
+
+@main
+struct YgoShuffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YgoShuffleTests/YgoShuffleTests.swift
@@ -0,0 +1,17 @@
+//
+//  YgoShuffleTests.swift
+//  YgoShuffleTests
+//
+//  Created by David Alonzo Medina on 8/17/25.
+//
+
+import Testing
+@testable import YgoShuffle
+
+struct YgoShuffleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
